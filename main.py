c422f97eb9bdafc956188e759769a25fd1f6dbbd
allan <abul0022@algonquinlive.com>
2024-08-06 19:23:55 -0400

    Update readme and main.py for logging

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import logging
 from src.data.load_dataset import load_and_preprocess_data, prepare_data
 from src.visualization.visualize import plot_scatter, plot_loss_curve
 from src.model.train_model import split_and_scale_data, train_mlp, perform_grid_search
 from src.model.predict_model import evaluate_model
 import warnings
+
+# Configure logging to write to both file and console
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('app.log'),
+        logging.StreamHandler()
+    ]
+)
+
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
